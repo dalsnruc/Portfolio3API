af511004d615773488df90be6483c9972ced32d5
dalsnruc <dals@ruc.dk>
2024-12-13 16:06:08 +0100

    Update

--- WebApi/Models/TitleModels/AllTitlesModel.cs
+++ WebApi/Models/TitleModels/AllTitlesModel.cs
@@ -7,6 +7,7 @@ namespace WebApi.Models.TitleModels
         public string? Url { get; set; }
         public string TitleType { get; set; }
         public string PrimaryTitle { get; set; } = string.Empty;
+        public string Genre {  get; set; }
         public PlotAndPosterModel PlotAndPoster { get; set; }
     }
 }
--- WebApi/Models/TitleModels/UserTitleModel.cs
+++ WebApi/Models/TitleModels/UserTitleModel.cs
@@ -2,6 +2,8 @@
 {
     public class UserTitleModel
     {
+        public string Url {  get; set; }
         public string PrimaryTitle { get; set; }
+        public string Genre {  get; set; }
     }
 }
